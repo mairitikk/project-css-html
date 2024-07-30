7354e9af3fe8c3a1966d5aff159981fbfd9a0ece
mairi tikk <mairi.tikk@gmail.com>
2024-07-30 09:59:28 +0200

    add button

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
-import Header from "./header"; 
+import Header from "./Header"; 
 import Footer from "./Footer";
 import Card from "./Card";
+import Button from "./button/Button";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Card></Card>
+      <Button></Button>
       <Footer></Footer>
     </>
   );
