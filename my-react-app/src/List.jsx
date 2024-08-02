73974bfb0297a99c008bdd0341c2d19155a2d474
mairi tikk <mairi.tikk@gmail.com>
2024-08-02 12:56:47 +0200

    add click events

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,18 +1,24 @@
 
 
 function List() {
-    const fruit = [{ name:"virsik", calories:95},
-        {id:1, name: "aprikoos", calories:10},
-        {id:2, name: "arbuus", calories:5},
-        {id:3, name: "mustiaks", calories: 7},
-        { id: 4, name: "vaarikas", calories: 100 }];
-    //fruit.sort((a,b) => a.name.localeCompare(b.name)) sorteerida t2hestikulises j2rjekorras
+   const fruit = [{ name:"virsik", calories:95},
+      {id:1, name: "aprikoos", calories:10},
+     {id:2, name: "arbuus", calories:5},
+      {id:3, name: "mustiaks", calories: 7},
+       { id: 4, name: "vaarikas", calories: 100 }];
+fruit.sort((a,b) => a.name.localeCompare(b.name)) 
     // fruit.sort((a,b) => b.name.localeCompare(a.name))
-    fruit.sort((a, b) => a.calories - b.calories);
+    //fruit.sort((a, b) => a.calories - b.calories);
     //
-    // eslint-disable-next-line react/jsx-key
-    const listItems = fruit.map(fruits => <li key={fruit.id}>{fruits.name}: &nbsp; <b>{ fruits.calories}</b></li> )
-    return (<ol>{ listItems}</ol>);
-}
 
+
+   
+
+     // eslint-disable-next-line react/jsx-key
+    const listItems = fruit.map(fruits => <li key={fruit.id}>{fruits.name}: &nbsp; <b>{ fruits.calories}</b></li> )
+    // const lowCalFruits = fruit.filter(fruit => fruit.calories < 100);
+    //const listItems = lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}> {lowCalFruit.name}: &nbsp; <b>{lowCalFruit.calories}</b></li>);
+    
+ return (<ol>{listItems}</ol>);
+}
 export default List
--- my-react-app/src/button/Button.jsx
+++ my-react-app/src/button/Button.jsx
@@ -1,8 +1,11 @@
 import styles from './Button.module.css'
 
 function Button() {
+
+    const handeleClick = () => console.log("OuCH!");
+    const handeleClick2 = (name) => console.log(`${name}LOL`);
     return (
-        <button className={styles.button}>Click me</button>
+        <button onClick={() => handeleClick2("Martin")} className={styles.button}>Click me</button>
     )
  }
 export default Button
