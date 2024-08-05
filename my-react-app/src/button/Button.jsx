fd67d5e2c1a8569505c69eb449ef0cf1e9239d8a
mairi tikk <mairi.tikk@gmail.com>
2024-08-05 11:19:20 +0200

    add changes click event

--- my-react-app/src/button/Button.jsx
+++ my-react-app/src/button/Button.jsx
@@ -1,11 +1,26 @@
 import styles from './Button.module.css'
 
 function Button() {
+   /* let count = 0;
 
-    const handeleClick = () => console.log("OuCH!");
-    const handeleClick2 = (name) => console.log(`${name}LOL`);
+    const handeleClick = (name) => {
+        if (count < 3) {
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`);
+        }
+        else {
+            console.log(`${name} stop clicking me!`);
+        }
+    } ;
+   // const handeleClick2 = (name) => console.log(`${name}LOL`);
     return (
-        <button onClick={() => handeleClick2("Martin")} className={styles.button}>Click me</button>
-    )
- }
+        <button onClick={() => handeleClick("Martin")} className={styles.button}>Click me</button>
+    ) */
+    
+    const handeleClick = (event) => event.target.textContent = "TERE :)";
+    return (<button onClick={(e) => handeleClick(e)} className={styles.button}>VAJUTA SIIA</button>
+    )  
+}
+
+
 export default Button
