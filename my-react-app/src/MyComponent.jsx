e59931df7f7558374edfea167b69753545f658bc
mairi tikk <mairi.tikk@gmail.com>
2024-08-06 14:44:08 +0200

    add age in my component

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,15 +1,23 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 function MyComponent() {
-    const [name, setName] = useState();
+    const [name, setName] = useState("Kulaline");
+    const [age, setAge] = useState(0);
     const updateName = () =>
     {
         setName("MinniMouse")
     }
+    const incrementAge = () =>{
+        setAge(age + 1);
+    }
     
-    return <div className="">
+    return(
+    <div className="">
         <p>Name: {name}</p>
-        <button onClick={updateName}>Set Name</button>
-    </div>
+            <button onClick={updateName}>Set Name</button>
+            
+            <p>Age: {age}</p>
+        <button onClick={incrementAge}>Set Age</button>
+    </div>)
 }
 export default MyComponent
